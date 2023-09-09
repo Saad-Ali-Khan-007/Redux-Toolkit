0d2e42d82e39d8ab9c3ba965c033bbe620c65332
Saad-Ali-Khan-007 <saadalikhan042@gmail.com>
2023-09-09 17:02:09 +0500

    first commit

---
+++ src/cartItems.js
@@ -0,0 +1,31 @@
+const cartItems = [
+  {
+    id: "rec1JZlfCIBOPdcT2",
+    title: "Samsung Galaxy S8",
+    price: "399.99",
+    img: "https://image.oppo.com/content/dam/oppo/common/mkt/v2-2/a76/middlebanner/a76-pc.png.thumb.webp",
+    amount: 1,
+  },
+  {
+    id: "recB6qcHPxb62YJ75",
+    title: "google pixel",
+    price: "499.99",
+    img: "https://kddi-h.assetsadobe3.com/is/image/content/dam/au-com/mobile/mb_img_58.jpg?scl=1",
+    amount: 1,
+  },
+  {
+    id: "recdRxBsE14Rr2VuJ",
+    title: "Xiaomi Redmi Note 2",
+    price: "699.99",
+    img: "https://image.oppo.com/content/dam/oppo/common/mkt/v2-2/a78-en/middlebanner/a78-640_640-green.png.thumb.webp",
+    amount: 1,
+  },
+  {
+    id: "recwTo160XST3PIoW",
+    title: "Samsung Galaxy S7",
+    price: "599.99 ",
+    img: "https://image.oppo.com/content/dam/oppo/common/mkt/v2-2/a76/middlebanner/a76-pc.png.thumb.webp",
+    amount: 1,
+  },
+];
+export default cartItems;
